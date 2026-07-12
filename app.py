3170ca0118408c8b3480068596de4c702a994801
UsharanidivyaB <usharanidivyabcsbs2024@jerusalemengg.ac.in>
2026-07-12 08:07:58 +0530

    Configured app for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from ml.predictor import predict_output
 
@@ -174,5 +175,11 @@ Biogas Digestion
     )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
